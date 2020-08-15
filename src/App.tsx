8af8153f0dbaa51a9175c5a686aa9a6ddbab997a
Dallas Carraher <dall133android@gmail.com>
2020-08-14 18:53:10 -0700

    some fun changes 🧽

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from "react";
-import background from "./img/bg.jpg";
+import conch from "./img/magicConch.jpeg";
 import { default as choices } from "./choices";
-import { default as randomize } from "./utils/random";
 import { default as Wheel } from "./components/Wheel/Wheel";
 
 export default function App() {
   return (
     <div style={styles.app}>
-      <h1>What should we play?</h1>
+      <h1 style={styles.title1}>Magic Conch, </h1>
+      <h1>what should we play?</h1>
       <Wheel items={choices} />
-      <p style={styles.imgCredit}>
-        <a
-          style={styles.link}
-          href="https://www.reddit.com/r/halo/comments/8dw8xs/mcc_splash_wallpaper_with_vertical_halo_1080p/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          img by The_Roptor
-        </a>
-      </p>
     </div>
   );
 }
@@ -31,14 +21,10 @@ const styles = {
     fontFamily: "Lato",
     color: "white",
     textAlign: "center" as "center",
-    backgroundImage: `url(${background})`,
+    backgroundImage: `url(${conch})`,
     backgroundSize: "cover",
   },
-  imgCredit: {
-    fontSize: 12,
-    margin: "5rem",
-  },
-  link: {
-    color: "white",
+  title1: {
+    marginBottom: 0,
   },
 };
